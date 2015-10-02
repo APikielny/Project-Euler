cccb3d2110712730ef947f3fb91ed15d20ded984
APikielny <apikielny@gmail.com>
2015-10-02 14:22:28 -0400

    Updated from Brython Server: October 2, 2015 2:22:28 PM EDT

--- Problem1.py
+++ Problem1.py
@@ -1,1 +1,10 @@
-
+#Adam Pikielny
+newlist=[]
+for x in range(0,1000):
+    if x%5==0 and x%3==0:
+        newlist.append(x)
+    elif x%5==0:
+        newlist.append(x)
+    elif x%3==0:
+        newlist.append(x)
+print(sum(newlist))
